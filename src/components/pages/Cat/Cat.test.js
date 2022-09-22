27dc15b617b2fa235635d4951c3074804b33fd0f
DanielMalede <danielmalede@gmail.com>
2022-09-22 16:27:23 +0300

    .

--- src/components/pages/Cat/Cat.test.js
+++ src/components/pages/Cat/Cat.test.js
@@ -1,13 +1,12 @@
 import { createElement } from "react";
-import {ReactDOM} from "react-dom";
-import {createRoot} from 'react-dom/client';
+import { ReactDOM } from "react-dom";
+import { createRoot } from "react-dom/client";
 import Cat from "./Cat";
 
-
-describe("test for Cat",()=>{
-  it("return element HTML and text cat",()=>{
-    const div = createElement('div')
-    ReactDOM.createRoot(div).render(<Cat/>)
-    expect(div.innerHTML).toEqual('<div class="cat"><h1>Cat</h1></div>')
-  })
-})
+describe("test for Cat", () => {
+  it("return element HTML and text cat", () => {
+    const div = createElement("div");
+    ReactDOM.createRoot(div).render(<Cat />);
+    expect(div.innerHTML).toEqual('<div class="cat"><h1>Cat</h1></div>');
+  });
+});
